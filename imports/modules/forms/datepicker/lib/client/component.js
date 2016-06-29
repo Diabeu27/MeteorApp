50545b08ad4ffbae1358c244ac933405da493a07
Diabeu27 <scibor.rudnicki@ster-project.pl>
2016-06-29 15:04:17 +0200

    refactor(modules/forms/datepicker): add collection, 4 spaces indent

--- imports/modules/forms/datepicker/lib/client/component.js
+++ imports/modules/forms/datepicker/lib/client/component.js
@@ -1,24 +1,21 @@
 import templateUrl from './view';
 import {ModuleName} from '/imports/modules/forms/lib/client/module';
-
-import '/imports/modules/forms/datepicker/lib/both/date.collection';
-import '/imports/modules/forms/datepicker/lib/both/collection';
-import {init,SetModule,State,Component,View,Inject,LocalInjectables} from 'angular2-now';
+import elements from '/imports/modules/forms/datepicker/api/collection';
+import {init,SetModule,State,Component,View,LocalInjectables} from 'angular2-now';
 
 init();
 SetModule(ModuleName)
 @State({
-  name: 'app.forms.datepicker',
-  url: '/datepicker'
+    name: 'app.forms.datepicker',
+    url: '/datepicker'
 })
 @Component({
-  selector: 'meteor-forms-datepicker'
-})
-@View({
-  templateUrl: templateUrl
+    selector: 'meteor-forms-datepicker',
+    templateUrl: templateUrl
 })
 @LocalInjectables
 export class FormsDatepickerComponent {
-  constructor(){
-  }
+    constructor(){
+        this.elements = elements;
+    }
 }
