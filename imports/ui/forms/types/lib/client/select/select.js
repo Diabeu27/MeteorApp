669d2454fbf252060a750b0209f64f2273891351
Diabeu27 <scibor.rudnicki@ster-project.pl>
2016-06-29 18:19:51 +0200

    refactor(ui/forms/types/select): add apiCheck, defaultOptions, wrapper, group add to view

--- imports/ui/forms/types/lib/client/select/select.js
+++ imports/ui/forms/types/lib/client/select/select.js
@@ -1,7 +1,45 @@
-import templateUrl from './view.html';
+import templateUrl from "./view.html";
 export default (formlyConfigProvider) => {
     formlyConfigProvider.setType({
         templateUrl: templateUrl,
-        name: 'select'
+        name: "select",
+        wrapper: ["labelWrapper","messagesWrapper","inputWrapper"],
+        defaultOptions: {
+            templateOptions: {
+                theme: "default"
+            },
+            ngModelAttrs: {
+                disabled: {
+                    attribute: "ng-disabled"
+                },
+                ariaLabel: {
+                    attribute: 'aria-label'
+                },
+                mdOnOpen: {
+                    statement: "md-on-open"
+                },
+                mdOnClose: {
+                    statement: "md-on-close"
+                },
+                mdContainerClass: {
+                    attribute: "md-container-class"
+                }
+            }
+        },
+        apiCheck: (check) => ({
+            templateOptions: {
+                theme: check.string,
+                placeholder: check.string.optional,
+                label: check.string.optional,
+                required: check.bool.optional,
+                disabled: check.bool.optional,
+                ariaLabel: check.string.optional,
+                mdOnOpen: check.func.optional,
+                mdOnClose: check.func.optional,
+                mdContainerClass: check.string.optional,
+                options: check.arrayOf(check.object).optional,
+                groups: check.arrayOf(check.object).optional
+            }
+        })
     });
 };
