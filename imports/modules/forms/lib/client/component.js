6000080622442bc2fe59195194cbcb2a0d85cb4a
Diabeu27 <scibor.rudnicki@ster-project.pl>
2016-06-29 15:03:26 +0200

    refactor(modules/forms): undisabled radio, checkbox, chips in view, add 4 spaces indent, add components

--- imports/modules/forms/lib/client/component.js
+++ imports/modules/forms/lib/client/component.js
@@ -1,29 +1,34 @@
 import templateUrl from './view';
 import {ModuleName} from '/imports/modules/forms/lib/client/module';
-
-// Forms input
+// Forms input examples
 import '/imports/modules/forms/input/lib/client/component';
+// Forms checkbox examples
+import '/imports/modules/forms/checkbox/lib/client/component';
+// Forms chips examples
+import '/imports/modules/forms/chips/lib/client/component';
+// Forms datepicker examples
+import '/imports/modules/forms/datepicker/lib/client/component';
+// Forms radio examples
+import '/imports/modules/forms/radio/lib/client/component';
+// Forms select examples
+import '/imports/modules/forms/select/lib/client/component';
 
 import {init,SetModule, State, Component, View, Inject, LocalInjectables} from 'angular2-now';
 init();
 SetModule(ModuleName);
 @State({
-  name: 'app.forms',
-  url: '/forms'
+    name: 'app.forms',
+    url: '/forms'
 })
 @Component({
-  selector: 'meteor-forms'
+    selector: 'meteor-forms',
+    templateUrl: templateUrl,
+    providers: [
+        '$mdSidenav'
+    ]
 })
-@View({
-  templateUrl: templateUrl
-})
-@Inject([
-  '$mdSidenav'
-])
 @LocalInjectables
 export class FormsComponent {
-  sidenav = {};
-
-  constructor($mdSidenav){
-  }
+    constructor(){
+    }
 }
