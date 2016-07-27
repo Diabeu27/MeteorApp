84f305d304ef2705eddc3970bb74167669da1daf
Diabeu27 <scibor.rudnicki@ster-project.pl>
2016-07-27 16:18:10 +0200

    feature(main): add hightlight default css

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
-import 'normalize.css';
+import "normalize.css";
+//import "prismjs/themes/prism-solarizedlight.css";
+import "highlight.js/styles/default.css";
 // MeteorAppModule
-import './main';
-import '/imports/startup/client/module';
-import '/imports/startup/client/app/component';
+import "./main";
+import "/imports/startup/client/module";
+import "/imports/startup/client/app/component";
