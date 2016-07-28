7c49a700325e4bbc640f95d4fcdfe015225ad0a4
Diabeu27 <scibor.rudnicki@ster-project.pl>
2016-07-28 16:51:25 +0200

    refactor(forms.input): move api to lib,  add helpers, add wrapper, use submit helper, add custom-template, change attributes in view

--- imports/modules/forms/input/lib/client/component.js
+++ imports/modules/forms/input/lib/client/component.js
@@ -1,29 +1,35 @@
-
-import templateUrl from './view';
-import {ModuleName} from '/imports/modules/forms/lib/client/module';
-import InputsCollection from '/imports/modules/forms/input/api/collection';
-import {init, SetModule, State, Component, View, Inject, LocalInjectables} from 'angular2-now';
+import templateUrl from "./view";
+import {ModuleName} from "/imports/modules/forms/lib/client/module";
+import InputsCollection from "/imports/modules/forms/input/lib/api/collection";
+import submit from "/imports/modules/forms/lib/client/helpers/on-submit";
+import {init, SetModule, State, Component, LocalInjectables} from "angular2-now";
 
 init();
 SetModule(ModuleName);
 @State({
-  name: 'app.forms.input',
-  url: '/input'
+    name: "app.forms.input",
+    url: "/input"
 })
 @Component({
-  selector: 'meteor-forms-element'
-})
-@View({
-  templateUrl: templateUrl
+    selector: "meteor-forms-element",
+    templateUrl: templateUrl
 })
 @LocalInjectables
 export class FormsElementComponent {
-  form = '';
-  model = {
-    firstname: 'Obi Wan'
-  };
-  elements = [];
-  constructor(){
-    this.elements = InputsCollection;
-  }
+    form = "inputForm";
+    model = {
+        firstname: "Obi Wan"
+    };
+    elements = [];
+    options = {
+        formState: {
+            disabled: true
+        }
+    };
+    constructor(){
+        this.elements = InputsCollection;
+    }
+    onSubmit = (name) => {
+        submit(name, this.elements);
+    }
 }
