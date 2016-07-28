2c47448a0a79ed4411a727d1b54e13ceb0771b3e
Diabeu27 <scibor.rudnicki@ster-project.pl>
2016-07-28 17:05:20 +0200

    refactor(forms/checkbox): add examples to collection, change attributes in view, use submit helper in component

--- imports/modules/forms/checkbox/lib/client/component.js
+++ imports/modules/forms/checkbox/lib/client/component.js
@@ -1,33 +1,31 @@
 
-import {ModuleName} from '/imports/modules/forms/lib/client/module';
-import templateUrl from './view';
-import elements from '/imports/modules/forms/checkbox/lib/api/collection';
-import {init, SetModule, State, Component, View, Inject, LocalInjectables} from 'angular2-now';
-
+import {ModuleName} from "/imports/modules/forms/lib/client/module";
+import templateUrl from "./view";
+import elements from "/imports/modules/forms/checkbox/lib/api/collection";
+import submit from "/imports/modules/forms/lib/client/helpers/on-submit";
+import {init, SetModule, State, Component, LocalInjectables} from "angular2-now";
 init();
 SetModule(ModuleName);
 @State({
-    name: 'app.forms.checkbox',
-    url: '/checkbox'
+    name: "app.forms.checkbox",
+    url: "/checkbox"
 })
 @Component({
-    selector: 'meteor-forms-checkbox',
+    selector: "meteor-forms-checkbox",
     templateUrl: templateUrl,
     providers: [
-        '$scope'
+        "$scope"
     ]
 })
 @LocalInjectables
 export class FormsCheckboxComponent {
+    form = "checkboxForm";
     model = {};
-    allChecked = false;
-    checkbox = {
-        '1': true,
-        '2': false,
-        '3': false,
-        '4': true
+    options = {
+        formState: {
+            disabled: false
+        }
     };
-
     constructor() {
         this.elements = elements;
     }
@@ -35,47 +33,15 @@ export class FormsCheckboxComponent {
     /*
         Submit form
     */
-    onSubmit = () => {
-        console.log(this.model);
-        alert(JSON.stringify(this.model), null, 2);
-    }
-
-    /*
-        Check is all checked
-    */
-    isChecked = (x) => {
-        this.allChecked = true;
-        for (var key in this.checkbox) {
-            if (this.checkbox[key] === false) {
-                this.allChecked = false;
-            }
-        }
-        return this.allChecked;
-    }
-
-    /*
-        Unselect/Select all
-    */
-    toggleAll = () => {
-        if (this.allChecked === false) {
-            for (var key in this.checkbox) {
-                this.checkbox[key] = true;
-            }
-            this.allChecked = true;
-        }
-        else {
-            for (var key in this.checkbox) {
-                this.checkbox[key] = false;
-            }
-            this.allChecked = false;
-        }
-    }
+    onSubmit = (name) => {
+        submit(name, this.elements);
+    };
 
     /*
         ---
     */
     isIndeterminate = () => {
-        console.log('isIndeterminate');
+        console.log("isIndeterminate");
         if (this.allChecked === false) {
             for (var key in this.checkbox) {
                 if (this.checkbox[key] === true) {
