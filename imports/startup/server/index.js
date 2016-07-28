5600985b01f00cb772c0beee6d869801808740e4
Diabeu27 <scibor.rudnicki@ster-project.pl>
2016-07-28 16:23:12 +0200

    refactor(startup): add import chat server, add higlight to helpers

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,2 +1,3 @@
-import '/imports/modules/todo/lib/server/fixtures';
-import '/imports/modules/todo/lib/server/publications';
+import "/imports/modules/todo/lib/server/fixtures";
+import "/imports/modules/todo/lib/server/publications";
+import "/imports/simple-chat/server";
